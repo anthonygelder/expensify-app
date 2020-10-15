83f757bd82f610c7f1e0e3ec79b430e658bdcc3d
Anthony Gelder <anthony.gelder@gmail.com>
2020-10-14 21:21:01 -0700

    breaking out components

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddExpensePage = () => {
+    return (
+        <div>
+            Add Expense
+        </div>
+    )
+}
+
+export default AddExpensePage
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <div>
+            This is a header
+            <NavLink exact to="/" activeClassName="is-active">Home</NavLink>
+            <NavLink to="/create" activeClassName="is-active">Create</NavLink>
+            <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        </div>
+    )
+}
+
+export default Header;
+
