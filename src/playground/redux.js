159fcf35ef6f7e624a49bf14ccb53e815ce23623
Anthony Gelder <anthony.gelder@gmail.com>
2020-10-16 22:14:37 -0700

    redux store and actions

--- src/playground/redux.js
+++ src/playground/redux.js
@@ -1,7 +1,49 @@
 import {createStore} from 'redux'
 
-const store = createStore((state = {count: 0}) => {
-    return state
+const store = createStore((state = {count: 0}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+            return {
+                count: state.count + incrementBy
+            }
+        case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
+            return {
+                count: state.count - decrementBy
+            }
+        case 'RESET':
+            return {
+                count: 0
+            }
+        default: state
+    }
 })
 
-console.log(store.getState())
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState())
+})
+
+// increment
+
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 10
+})
+store.dispatch({
+    type: 'INCREMENT'
+})
+store.dispatch({
+    type: 'INCREMENT'
+})
+store.dispatch({
+    type: 'RESET'
+})
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+})
+
+
+
