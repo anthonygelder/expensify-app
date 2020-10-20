5191cb79a193e425e550dcad6358faf3f9e41934
Anthony Gelder <anthony.gelder@gmail.com>
2020-10-19 21:37:08 -0700

    reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,59 @@
 import { createStore, combineReducers } from 'redux'
+import uuid from 'uuid'
 
+// ADD_EXPENSE
+const addExpense = ({description = '', note = '', amount = 0, createAt = 0}) => {
+    return (
+        {
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: uuid(),
+                description,
+                note,
+                amount,
+                createAt
+            }
+        }
+    )
+}
+
+// REMOVE_EXPENSE
+
+const removeExpense = ({id} = {}) => {
+    return (
+        {
+            type: 'REMOVE_EXPENSE',
+            id
+        }
+    )
+}
+// EDIT_EXPENSE
+
+const editExpense = (id, update) => {
+    return (
+        {
+            type: 'EDIT_EXPENSE',
+            id,
+            update
+        }
+    )
+}
+
+// SET_TEXT_FILTER
+
+const setTextFilter = (text = '') => {
+    return (
+        {
+            type: 'SET_TEXT_FILTER',
+            text
+        }
+    )
+}
+
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
 
 // Expenses reducer
 
@@ -7,6 +61,21 @@ const expensesReducerDefaultState = []
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE': 
+            return [...state, action.expense]
+        case 'REMOVE_EXPENSE':
+            return state.filter(expense => expense.id !== action.id)
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if(expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.update
+                    }
+                } else {
+                    return expense
+                }
+            }) 
         default: 
             return state
     }
@@ -21,6 +90,11 @@ const filtersRecuderDefaultState = {
 
 const filtersReducer = (state = filtersRecuderDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            }
         default: 
             return state
     }
@@ -33,7 +107,19 @@ const store = createStore(combineReducers({
     filters: filtersReducer
 }))
 
-console.log(store.getState())
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
+const expenseOne = store.dispatch(addExpense({description: 'rent', amount: 100}))
+const expenseTwo = store.dispatch(addExpense({description: 'place', amount: 100}))
+
+store.dispatch(removeExpense({id: expenseOne.expense.id}))
+
+store.dispatch(editExpense(expenseTwo.expense.id, {amount: 300}))
+
+store.dispatch(setTextFilter('rent'))
+store.dispatch(setTextFilter())
 
 const demoState ={
     expenses: [{
@@ -46,7 +132,7 @@ const demoState ={
     filters: {
         text: 'rent',
         sortBy: 'amount', // date or amount
-        startDate: underfined,
+        startDate: undefined,
         endDate: undefined
     }
 }
